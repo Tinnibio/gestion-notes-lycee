9c4bd2262970c1cfac7a7c333d2f197067b2b455
Tinnibio <charifoutinni@gmail.com>
2026-08-13 12:32:07 +0000

    chngments

--- app.py
+++ app.py
@@ -20,6 +20,40 @@ app.config['SQLALCHEMY_DATABASE_URI'] = DATABASE_URL
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
+
+
+import os
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+def create_app():
+    app = Flask(__name__)
+
+    database_url = os.environ.get("DATABASE_URL")
+
+    if database_url and database_url.startswith("postgres://"):
+        database_url = database_url.replace("postgres://", "postgresql://", 1)
+
+    app.config["SQLALCHEMY_DATABASE_URI"] = database_url
+    app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+
+    db.init_app(app)
+
+@app.route("/")
+    def home():
+        return "App OK"
+
+    return app
+
+app = create_app()
+
+
+
+
+
+
 # ==================== MODÈLES ====================
 
 class Annee(db.Model):
